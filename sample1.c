e78953f390ed4e0d962c3f196efeeec4a1dec0bd
shreyu <shreyasais12@gmail.com>
2024-03-05 11:40:28 +0530

    Updated sample1.c file

--- sample1.c
+++ sample1.c
@@ -3,5 +3,6 @@ int main()
 {
 	printf("I am in sample1.c file");
 	printf("I am adding this line remotely");//Added this line remotely
+	printf("I am adding this line after pulling remotely in local");//Adding this line locally
 	return 0;
 }
